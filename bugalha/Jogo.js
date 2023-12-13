1781bb9e1a97132bca417d403fa26371b3e90569
Larissa <10475169697@estudante.ufjf.br>
2023-12-13 20:48:46 -0300

    Cria estutura inicial do jogo

---
+++ bugalha/Jogo.js
@@ -0,0 +1,38 @@
+const jogo = {
+    'jogador1': {
+        tabuleiro: [[]]
+    },
+    'jogador2': {
+        tabuleiro: [[]]
+    },
+    'turno': 1
+}
+
+function iniciarJogo() {
+    // Iniciar os tabuleiros
+    // Definir quem vai primeiro
+}
+
+function iniciarRodada () {
+    // Rolar um dado
+    // Mostrar colunas disponíveis
+}
+
+function fazerJogada(coluna) {
+    // atualiza os valores do tabuleiro
+    // Atualiza tabuleiro de adversário
+    // Ao fim, calcula pontuacao
+    // Verifica fim de jogo
+    // Muda vez
+}
+
+// Atualiza dados no tabuleiro
+function atualizaTabuleiro () {
+
+}
+
+
+function calcularPontuacao() {
+
+}
+
